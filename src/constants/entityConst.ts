7b2f652a4799d19b87a9dc043b1108222633a614
Jiahua Jiang <jiahua212@gmail.com>
2023-08-19 15:27:42 -0700

    .

---
+++ src/constants/entityConst.ts
@@ -0,0 +1,30 @@
+export type ProjectileConst = {
+    DAMAGE : number,
+    FIRE_RATE : number,
+    STUN_STRENGTH : number,
+    KNOCKBACK_STRENGTH : number,
+    PROJECTILE_SPEED : number,
+}
+
+// export const Fist_Stat: EntityConst = {
+//     DAMAGE : 1,
+//     FIRE_RATE : 300,
+//     STUN_STRENGTH : 100,
+//     KNOCKBACK_STRENGTH : 100,
+//     PROJECTILE_SPEED : 100,
+// };
+
+export const BugSpray_Stats: ProjectileConst = {
+    DAMAGE : 1,
+    FIRE_RATE : 1000,
+    STUN_STRENGTH : 800,
+    KNOCKBACK_STRENGTH : 150,
+    PROJECTILE_SPEED :250,
+}
+export const BugASalt_Stats: ProjectileConst = {
+    DAMAGE : 1,
+    FIRE_RATE : 1000,
+    STUN_STRENGTH : 150,
+    KNOCKBACK_STRENGTH : 700,
+    PROJECTILE_SPEED : 1000,
+}
